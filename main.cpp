4b002f16f133d5b602fb3a919ca8ca05cedd46b3
Esr0g <gorse.sacha@gmail.com>
2023-10-10 21:32:41 +0200

    Adding custom cursor

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QCursor cursor(QPixmap(":/images/main_cursor.png"));
+    QApplication::setOverrideCursor(cursor);
+
     MainWindow w;
     w.show();
 
