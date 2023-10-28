cb808b410ccd450141efe06ce752015421381ac3
Esr0g <gorse.sacha@gmail.com>
2023-10-28 11:37:12 +0200

    Add an example of creating an effect directly in the code at the end of haptichandler.cpp + add an exemple of modifying the effect scene_1

--- haptichandler.cpp
+++ haptichandler.cpp
@@ -19,6 +19,16 @@ HapticHandler::HapticHandler(MainWindow *fen, QObject *parent) : QObject(parent)
         qDebug() << "===> Chargement réussi ! :)";
 
         mVibreur = mProject->CreateEffect("scene_1", mSouris, IMM_PARAM_NODOWNLOAD);
+
+        // We modify the parameters : apparently they are not correctly charged from ImmStudio
+       CImmFriction *myEffect = static_cast<CImmFriction*>(mVibreur->GetContainedEffect(0L));
+        myEffect->ChangeNegativeCoefficient(8000);
+        myEffect->ChangePositiveCoefficient(8000);
+        myEffect->ChangeNegativeSaturation(10000);
+        myEffect->ChangePositiveSaturation(10000);
+        myEffect->ChangeMinVelocity(0);
+        myEffect->ChangeDirection(IMM_EFFECT_AXIS_BOTH);
+
         if (!mVibreur) {
             qDebug()<<"===>Erreur chargement eau ";
             delete mVibreur;
@@ -36,7 +46,7 @@ HapticHandler::HapticHandler(MainWindow *fen, QObject *parent) : QObject(parent)
   * @brief Start the vibratons of the haptic mouse
   */
  void HapticHandler::startVibrator() {
-    mVibreur->Start();
+   mVibreur->Start();
  }
 
  /**
@@ -53,4 +63,17 @@ HapticHandler::HapticHandler(MainWindow *fen, QObject *parent) : QObject(parent)
      if (mSouris) {
          delete mSouris;
      }
+
+     /**
+       This is an exemple of creating an Effect
+
+         CImmFriction *effect = new CImmFriction();
+         effect->Initialize(
+                     this->mSouris,
+                     8000,
+                     IMM_FRICTION_DEFAULT_MIN_VELOCITY,
+                     IMM_EFFECT_AXIS_BOTH,
+                     IMM_EFFECT_DEFAULT_DIRECTION_X,
+                     IMM_EFFECT_DEFAULT_DIRECTION_Y);
+    **/
  }
